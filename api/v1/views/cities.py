72cf65624823365e429aa30ac8f5538a51da262e
AndZapata <andzapata514@gmail.com>
2019-09-11 21:12:10 +0000

    [Fix] file cities model

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -66,7 +66,7 @@ def cities_json(city_id=None):
             try:
                 single_dict = storage.get('City', city_id)
                 for key, val in req.items():
-                    if key not in ["id", "created_at", "updated_at"]:
+                    if key not in ["id", "created_at", "updated_at", "state_id"]:
                         setattr(single_dict, key, val)
                     else:
                         pass
